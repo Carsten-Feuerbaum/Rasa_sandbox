1d86d69b537e101eccee886a62e62f846c910681
LSE <leon.sienknecht@opitz-consulting.com>
2021-05-07 11:50:52 +0200

    finance bot start

--- actions/actions.py
+++ actions/actions.py
@@ -5,53 +5,53 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-from typing import Any, Text, Dict, List, Union
+# from typing import Any, Text, Dict, List, Union
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.forms import FormAction
 
-class HealthForm(FormAction):
+# class HealthForm(FormAction):
 
-    def name(self):
-        return "health_form"
+#     def name(self):
+#         return "health_form"
 
-    @staticmethod
-    def required_slots(tracker):
+#     @staticmethod
+#     def required_slots(tracker):
 
-        if tracker.get_slot("confirm_exercise") == True:
-            return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
-        else:
-            return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
+#         if tracker.get_slot("confirm_exercise") == True:
+#             return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
+#         else:
+#             return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
 
-    def submit(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ) -> List[Dict]:
-        return []
+#     def submit(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any]
+#     ) -> List[Dict]:
+#         return []
 
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        return {
-            "confirm_exercise": [
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-                self.from_intent(intent="infomr", value=True),
-            ],
-            "sleep": [
-                self.from_entity(entity="sleep"),
-                self.from_intent(intent="deny", value="None")
-            ],
-            "diet": [
-                self.from_text(intent="inform"),
-                self.from_text(intent="affirm"),
-                self.from_text(intent="deny"),
-            ],
-            "goal": [
-                self.from_text(intent="inform"),
-            ]
-        }
+#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+#         return {
+#             "confirm_exercise": [
+#                 self.from_intent(intent="affirm", value=True),
+#                 self.from_intent(intent="deny", value=False),
+#                 self.from_intent(intent="infomr", value=True),
+#             ],
+#             "sleep": [
+#                 self.from_entity(entity="sleep"),
+#                 self.from_intent(intent="deny", value="None")
+#             ],
+#             "diet": [
+#                 self.from_text(intent="inform"),
+#                 self.from_text(intent="affirm"),
+#                 self.from_text(intent="deny"),
+#             ],
+#             "goal": [
+#                 self.from_text(intent="inform"),
+#             ]
+#         }
 
 
 
