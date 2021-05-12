f0685759b924d1c108ed3ddba40047d70ee845b2
LSE <leon.sienknecht@opitz-consulting.com>
2021-05-12 16:55:45 +0200

    first custom action

--- actions/actions.py
+++ actions/actions.py
@@ -1,78 +1,19 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+import datetime as dt
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# from typing import Any, Text, Dict, List, Union
+class ActionHelloWorld(Action):
 
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# from rasa_sdk.forms import FormAction
+    def name(self) -> Text:
+        return "action_show_time"
 
-# class HealthForm(FormAction):
+    def run(self, dispatcher: CollectingDispatcher, 
+            tacker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-#     def name(self):
-#         return "health_form"
+        dispatcher.utter_message(text=f"{dt.datetime.now()}")
 
-#     @staticmethod
-#     def required_slots(tracker):
-
-#         if tracker.get_slot("confirm_exercise") == True:
-#             return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
-#         else:
-#             return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
-
-#     def submit(
-#         self,
-#         dispatcher: CollectingDispatcher,
-#         tracker: Tracker,
-#         domain: Dict[Text, Any]
-#     ) -> List[Dict]:
-#         return []
-
-#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-#         return {
-#             "confirm_exercise": [
-#                 self.from_intent(intent="affirm", value=True),
-#                 self.from_intent(intent="deny", value=False),
-#                 self.from_intent(intent="infomr", value=True),
-#             ],
-#             "sleep": [
-#                 self.from_entity(entity="sleep"),
-#                 self.from_intent(intent="deny", value="None")
-#             ],
-#             "diet": [
-#                 self.from_text(intent="inform"),
-#                 self.from_text(intent="affirm"),
-#                 self.from_text(intent="deny"),
-#             ],
-#             "goal": [
-#                 self.from_text(intent="inform"),
-#             ]
-#         }
-
-
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        return []
