ba0082763cf00f0fed207e3195cc71a76d67c0f0
LSE <leon.sienknecht@opitz-consulting.com>
2021-05-17 13:13:26 +0200

    implemented confidence score with custom action to find users intent

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,8 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+
 
 
 class ActionHelloWorld(Action):
@@ -17,3 +19,67 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text=f"{dt.datetime.now()}")
 
         return []
+
+
+
+class ActionCustomFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "custom_fallback_action"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Hi Leon!")
+
+        # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
+
+        
+class ActionAskAffirmation(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_ask_affirmation"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        intent1 = tracker.latest_message['intent_ranking'][0].get("name")
+        intent2 = tracker.latest_message['intent_ranking'][1].get("name")
+        intent3 = tracker.latest_message['intent_ranking'][2].get("name")
+
+        confidence1 = tracker.latest_message['intent_ranking'][0].get("confidence")
+        confidence2 = tracker.latest_message['intent_ranking'][1].get("confidence")
+        confidence3 = tracker.latest_message['intent_ranking'][2].get("confidence")
+
+        string1 = "Meintest du " + str(intent1) + "? " + "Sicherheit: " + str(confidence1) + "."
+        string2 = "Meintest du " + str(intent2) + "? " + "Sicherheit: " + str(confidence2) + "."
+        string3 = "Meintest du " + str(intent3) + "? " + "Sicherheit: " + str(confidence3) + "."
+
+        payload1 = "/" + str(intent1)
+        payload2 = "/" + str(intent2)
+        payload3 = "/" + str(intent3)
+
+        buttons = []
+
+        buttons.append({'title': string1, 'payload': payload1 })
+        if confidence2 > 0.3:
+            buttons.append({'title': string2, 'payload': payload2 })
+        if confidence3 > 0.3:
+            buttons.append({'title': string3, 'payload': payload3 })
+
+
+        dispatcher.utter_message(text= "Ich bin mir nicht ganz sicher" , buttons=buttons)
+
+        return [UserUtteranceReverted()]
